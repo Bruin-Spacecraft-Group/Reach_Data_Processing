ba1e0c183996e5cfdf081dd0ebb28ed1ca9d3913
theurerjohn3 <theurerjohn3@gmail.com>
2018-02-28 18:06:11 -0800

    Update main.py

--- main.py
+++ main.py
@@ -53,9 +53,9 @@ velocity = np.matrix([0,0,0]).T
 position = np.matrix([0,0,0]).T
 
 #TODO function for dynamicaly assessing calibration constants
-ACCX_CALIB = -20
-ACCY_CALIB = -10
-ACCZ_CALIB = 13
+ACCX_CALIB = 0
+ACCY_CALIB = 0
+ACCZ_CALIB = 0
 
 #set positions of data in incoming csv packet
 TIMESTAMP = 0
@@ -71,6 +71,10 @@ MAGZ = 9
 MAGHEAD = 10
 TEMP = 11
 ALTITUDE = 12
+
+tots_not_launch = 1
+count =0
+
 
 #TODO 
 while ser.isOpen():
@@ -100,123 +104,135 @@ while ser.isOpen():
 	'''
 	data = dataString.split(",")
 
-	#had problems with only reading in a few data 
-	if (len(data) < 6):
-		print "not enough data"
-		continue
-
-	#establish spacecraft time
-	if(FIRST == True):
-		FIRST = False
-		oldtime = float(data[TIMESTAMP])
-		#since dt cannot be established, skip
-		#TODO save first data packet's raw data
-		continue
-
-	#DO STUFF WITH DATA
-	#convert from string type
-	for i in range(len(data)-1):
-		data[i] = float(data[i])
-	'''
-	if (len(data) < 12):
-		gpsRecieved = False
-		print "no coordinates"
-	else: 
-		gpsRecieved = True
-	'''
-	#establish time elapsed
-	dt = (data[TIMESTAMP] - oldtime)/1000 #convert ms to s
-	oldtime = data[TIMESTAMP]
-
-	#append altitude calculated from pressure
-	#data.append(altitudeCalc(data[1]))
-
-	#process acceleration
-	acceleration = accel.findInertialFrameAccel(data[ACCELX], data[ACCELY], data[ACCELZ], data[GYROX], data[GYROY], data[GYROZ], dt, [ACCX_CALIB, ACCY_CALIB, ACCZ_CALIB])
+	if(count<6):
+		ACCX_CALIB += data[ACCELX]
+		ACCY_CALIB += data[ACCELY]
+		ACCZ_CALIB += data[ACCELZ]
+		count++
+	else:
+		if(count==6):
+			ACCX_CALIB = ACCX_CALIB/6
+			ACCY_CALIB = ACCY_CALIB/6
+			ACCZ_CALIB = ACCZ_CALIB/6
+			count++
 	
-	#integrate to find velocity
-	velocity = velocity+acceleration*dt
-
-	#integrate to find position
-	position = position + velocity*dt
-
-	#append inertial frame acceleration velocity 
-	#and position data to transmitted data
-	data.append(acceleration.item(0))
-	data.append(acceleration.item(1))
-	data.append(acceleration.item(2))
-	data.append(velocity.item(0)) 
-	data.append(velocity.item(1))
-	data.append(velocity.item(2))
-	data.append(position.item(0))
-	data.append(position.item(1))
-	data.append(position.item(2))
-
-	'''
-	#Process GPS coordinates
-	if data[9] != oldGPSTime:
-		longitude = data[10]
-		latitude = data[11]
-		latDeg = math.floor(latitude)
-		lonDeg = math.floor(longitude)
-
-		latMin = float(latitude) - latDeg
-		lonMin = float(longitude) - lonDeg
-	    
-		lat = float(latDeg)+float(latMin)/60
-	 	lon = float(lonDeg)-float(lonMin)/60
-	    
-		saveCoor(lon, lat, data[12])
-	    
-	    #speed calculation from gps data:
-		#push the last new value to the old and then set the new value 
-		lat1 = lat2
-	 	lat2 = lat
-		lon1 = lon2
-		lon2 = lon
-
-		dt = data[9] - oldGPSTime
-		#specific to GPS because GPS not expected as often
-		oldGPSTime = data[9]
-		#calcVelGPS(lat1, lon1, lat2, lon2, dt)
-
-		print "sending:"
-	'''
+		#had problems with only reading in a few data 
+		if (len(data) < 6):
+			print "not enough data"
+			continue
 	
-	#append absolute time
-	data.append(time.time())
+		#establish spacecraft time
+		if(FIRST == True):
+			FIRST = False
+			oldtime = float(data[TIMESTAMP])
+			#since dt cannot be established, skip
+			#TODO save first data packet's raw data
+			continue
 	
-	finalData = ""
-	for i in range(len(data)-1):
-		#print data[i]
-		finalData = finalData + str(data[i]) + ", "
+		#DO STUFF WITH DATA
+		#convert from string type
+		for i in range(len(data)-1):
+			data[i] = float(data[i])
 		'''
-		finalData contents should be as follows, 
-		as a string separated by commas:
-		relative spacecraft time 
-		raw accelX
-		raw accelY
-		raw accelZ
-		gyroX
-		gyroY
-		gyroZ
-		magX
-		magY
-		magZ
-		magHead
-		temp(C)
-		altitude
-		inertial accelX
-		inertial accelY
-		inertial accelZ
-		velX
-		velY
-		velZ
-		posX
-		posY
-		posZ
-		absTime
+		if (len(data) < 12):
+			gpsRecieved = False
+			print "no coordinates"
+		else: 
+			gpsRecieved = True
 		'''
-	print finalData
-	#sock.sendto(finalData, (SEND_TO_IP, SEND_TO_PORT))
+		#establish time elapsed
+		dt = (data[TIMESTAMP] - oldtime)/1000 #convert ms to s
+		oldtime = data[TIMESTAMP]
+	
+		#append altitude calculated from pressure
+		#data.append(altitudeCalc(data[1]))
+	
+		#process acceleration
+		acceleration = accel.findInertialFrameAccel(data[ACCELX], data[ACCELY], data[ACCELZ], data[GYROX], data[GYROY], data[GYROZ], dt, [ACCX_CALIB, ACCY_CALIB, ACCZ_CALIB])
+		
+		#integrate to find velocity
+		velocity = velocity+acceleration*dt
+	
+		#integrate to find position
+		position = position + velocity*dt
+	
+		#append inertial frame acceleration velocity 
+		#and position data to transmitted data
+		data.append(acceleration.item(0))
+		data.append(acceleration.item(1))
+		data.append(acceleration.item(2))
+		data.append(velocity.item(0)) 
+		data.append(velocity.item(1))
+		data.append(velocity.item(2))
+		data.append(position.item(0))
+		data.append(position.item(1))
+		data.append(position.item(2))
+	
+		'''
+		#Process GPS coordinates
+		if data[9] != oldGPSTime:
+			longitude = data[10]
+			latitude = data[11]
+			latDeg = math.floor(latitude)
+			lonDeg = math.floor(longitude)
+	
+			latMin = float(latitude) - latDeg
+			lonMin = float(longitude) - lonDeg
+		    
+			lat = float(latDeg)+float(latMin)/60
+		 	lon = float(lonDeg)-float(lonMin)/60
+		    
+			saveCoor(lon, lat, data[12])
+		    
+		    #speed calculation from gps data:
+			#push the last new value to the old and then set the new value 
+			lat1 = lat2
+		 	lat2 = lat
+			lon1 = lon2
+			lon2 = lon
+
+			dt = data[9] - oldGPSTime
+			#specific to GPS because GPS not expected as often
+			oldGPSTime = data[9]
+			#calcVelGPS(lat1, lon1, lat2, lon2, dt)
+	
+			print "sending:"
+		'''
+		
+		#append absolute time
+		data.append(time.time())
+		
+		finalData = ""
+		for i in range(len(data)-1):
+			#print data[i]
+			finalData = finalData + str(data[i]) + ", "
+			'''
+			finalData contents should be as follows, 
+			as a string separated by commas:
+			relative spacecraft time 
+			raw accelX
+			raw accelY
+			raw accelZ
+			gyroX
+			gyroY
+			gyroZ
+			magX
+			magY
+			magZ
+			magHead
+			temp(C)
+			altitude
+			inertial accelX
+			inertial accelY
+			inertial accelZ
+			velX
+			velY
+			velZ
+			posX
+			posY
+			posZ
+			absTime
+			'''
+		print finalData
+		#sock.sendto(finalData, (SEND_TO_IP, SEND_TO_PORT))
 #killSocket(sock)
